94a09355b255ca4cf0614496f906b496c311fd9a
장정우 <enterfive@gmail.com>
2024-05-07 19:09:36 +0900

    20240507 개발

--- validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -1,12 +1,10 @@
 package hello.itemservice.validation;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.ObjectError;
 
 public class MessageCodesResolverTest {
 
@@ -17,10 +15,26 @@ public class MessageCodesResolverTest {
         String[] messageCodes = messageCodesResolver.resolveMessageCodes("required", "item");
 
         for (String messageCode : messageCodes) {
-            System.out.println("messageCodes = " + messageCodes);
+            System.out.println("messageCode = " + messageCode);
         }
 
         assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageResolverField() {
+        String[] messageCodes = messageCodesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+
+        assertThat(messageCodes).containsExactly(
+            "required.item.itemName",
+            "required.itemName",
+            "required.java.lang.String",
+            "required"
+        );
+
     }
 
 }
