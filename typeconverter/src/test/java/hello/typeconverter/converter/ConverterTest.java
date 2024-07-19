7e2df48fcfc975a399657e416a6f37dff207d79e
장정우 <jungwoo@lotte.net>
2024-07-19 16:44:24 +0900

    20240719 스프링 타입 컨버터

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package hello.typeconverter.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void IntegerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+}
