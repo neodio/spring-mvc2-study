62908d95f71b0587dfdf1033fb685165d1d16d5c
장정우 <jungwoo@lotte.net>
2024-07-23 18:42:05 +0900

    20240723 컨버전 서비스 - ConversionService

--- typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -2,6 +2,7 @@ package hello.typeconverter.converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hello.typeconverter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 public class ConverterTest {
@@ -18,6 +19,22 @@ public class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
     }
 
 }
